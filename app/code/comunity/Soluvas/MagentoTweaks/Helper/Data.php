a877e4d3051c20453bfe2b2a8aa740fadbc76c05
Hendy Irawan <hendy@soluvas.com>
2010-07-12 15:06:17 +0700

    Initial version. Configuration does not take effect yet.

---
+++ app/code/comunity/Soluvas/MagentoTweaks/Helper/Data.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Soluvas
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ *
+ * @category   Soluvas
+ * @package    Soluvas_MagentoTweaks
+ * @copyright  Copyright (c) 2010 Soluvas (http://www.soluvas.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Soluvas MagentoTweaks Data helper
+ *
+ * @category   Soluvas
+ * @package    Soluvas_MagentoTweaks
+ * @author     Soluvas Developers <info@soluvas.com>
+ */
+class Soluvas_MagentoTweaks_Helper_Data extends Mage_Core_Helper_Abstract
+{
+	
+}
---
+++ app/code/comunity/Soluvas/MagentoTweaks/Sales/Quote.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Soluvas
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ *
+ * @category   Soluvas
+ * @package    Soluvas_MagentoTweaks
+ * @copyright  Copyright (c) 2010 Soluvas (http://www.soluvas.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Quote model that defaults "Ship to this address".
+ */
+class Soluvas_MagentoTweaks_Sales_Quote extends Mage_Sales_Model_Quote
+{
+
+    /**
+     * Retrieve quote address by type
+     *
+     * @param   string $type
+     * @return  Mage_Sales_Model_Quote_Address
+     */
+    protected function _getAddressByType($type)
+    {
+    	Mage::log("Soluvas_MagentoTweaks_Sales_Quote::_getAddressByType($type)");
+        foreach ($this->getAddressesCollection() as $address) {
+            if ($address->getAddressType() == $type && !$address->isDeleted()) {
+                return $address;
+            }
+        }
+        $address = Mage::getModel('sales/quote_address')->setAddressType($type);
+        if ($type == Mage_Sales_Model_Quote_Address::TYPE_SHIPPING) {
+        	Mage::log("Soluvas_MagentoTweaks_Sales_Quote::_getAddressByType: set SameAsBilling=1");
+        	$address->setSameAsBilling(1);
+        }
+        $this->addAddress($address);
+        return $address;
+    }
+	
+}
