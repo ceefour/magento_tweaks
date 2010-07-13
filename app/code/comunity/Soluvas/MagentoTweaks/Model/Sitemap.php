2561d85e53f7113dd0461768501f5ba301562b8d
Hendy Irawan <hendy@soluvas.com>
2010-07-13 16:26:21 +0700

    Google Sitemap tweaks

---
+++ app/code/comunity/Soluvas/MagentoTweaks/Model/Sitemap.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * Soluvas
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ *
+ * @category   Soluvas
+ * @package    Soluvas_MagentoTweaks
+ * @copyright  Copyright (c) 2010 Soluvas (http://www.soluvas.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Enhanced Google Sitemap with some critical tweaks:
+ * - Include Front page
+ * - Exclude 'home' CMS page
+ * - Exclude 'enable-cookies' CMS page
+ *
+ * @category   Soluvas
+ * @package    Soluvas_MagentoTweaks
+ * @author     Soluvas Developers <info@soluvas.com>
+ */
+class Soluvas_MagentoTweaks_Model_Sitemap extends Mage_Sitemap_Model_Sitemap
+{
+
+    /**
+     * Generate XML file
+     *
+     * @return Mage_Sitemap_Model_Sitemap
+     */
+    public function generateXml()
+    {
+        $io = new Varien_Io_File();
+        $io->setAllowCreateFolders(true);
+        $io->open(array('path' => $this->getPath()));
+
+        if ($io->fileExists($this->getSitemapFilename()) && !$io->isWriteable($this->getSitemapFilename())) {
+            Mage::throwException(Mage::helper('sitemap')->__('File "%s" cannot be saved. Please, make sure the directory "%s" is writeable by web server.', $this->getSitemapFilename(), $this->getPath()));
+        }
+
+        $io->streamOpen($this->getSitemapFilename());
+
+        $io->streamWrite('<?xml version="1.0" encoding="UTF-8"?>' . "\n");
+        $io->streamWrite('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">');
+
+        $storeId = $this->getStoreId();
+        $date    = Mage::getSingleton('core/date')->gmtDate('Y-m-d');
+        $baseUrl = Mage::app()->getStore($storeId)->getBaseUrl(Mage_Core_Model_Store::URL_TYPE_LINK);
+
+        /**
+         * Generate front page sitemap.
+         * Front page is slightly less important than category pages, but constantly updated
+         * and more important than other CMS pages.
+         * (we want category pages to be shown more on SERPs than front page)
+         * Calculation below will yield priority of 0.4, assuming category pages are assigned priority 0.5 (default).
+         */
+        if (Mage::getStoreConfig('magentotweaks/googlesitemap/front', $storeId) == 1) {
+	        $changefreq = 'daily';
+	        $priority   = (string)(Mage::getStoreConfig('sitemap/category/priority', $storeId) * 0.8);
+	        $xml = sprintf('<url><loc>%s</loc><lastmod>%s</lastmod><changefreq>%s</changefreq><priority>%.1f</priority></url>',
+	            htmlspecialchars($baseUrl),
+	            $date,
+	            $changefreq,
+	            $priority
+	        );
+	        $io->streamWrite($xml);
+        }
+        
+        /**
+         * Generate categories sitemap
+         */
+        $changefreq = (string)Mage::getStoreConfig('sitemap/category/changefreq', $storeId);
+        $priority   = (string)Mage::getStoreConfig('sitemap/category/priority', $storeId);
+        $collection = Mage::getResourceModel('sitemap/catalog_category')->getCollection($storeId);
+        foreach ($collection as $item) {
+            $xml = sprintf('<url><loc>%s</loc><lastmod>%s</lastmod><changefreq>%s</changefreq><priority>%.1f</priority></url>',
+                htmlspecialchars($baseUrl . $item->getUrl()),
+                $date,
+                $changefreq,
+                $priority
+            );
+            $io->streamWrite($xml);
+        }
+        unset($collection);
+
+        /**
+         * Generate products sitemap
+         */
+        $changefreq = (string)Mage::getStoreConfig('sitemap/product/changefreq', $storeId);
+        $priority   = (string)Mage::getStoreConfig('sitemap/product/priority', $storeId);
+        $collection = Mage::getResourceModel('sitemap/catalog_product')->getCollection($storeId);
+        foreach ($collection as $item) {
+            $xml = sprintf('<url><loc>%s</loc><lastmod>%s</lastmod><changefreq>%s</changefreq><priority>%.1f</priority></url>',
+                htmlspecialchars($baseUrl . $item->getUrl()),
+                $date,
+                $changefreq,
+                $priority
+            );
+            $io->streamWrite($xml);
+        }
+        unset($collection);
+
+        /**
+         * Generate cms pages sitemap
+         */
+        $changefreq = (string)Mage::getStoreConfig('sitemap/page/changefreq', $storeId);
+        $priority   = (string)Mage::getStoreConfig('sitemap/page/priority', $storeId);
+        $collection = Mage::getResourceModel('sitemap/cms_page')->getCollection($storeId);
+        foreach ($collection as $item) {
+        	// skip home
+        	if (Mage::getStoreConfig('magentotweaks/googlesitemap/excludehome', $storeId) == 1 && $item->getUrl() == 'home')
+        		continue;
+        	// skip enable-cookies
+        	if (Mage::getStoreConfig('magentotweaks/googlesitemap/excludecookies', $storeId) == 1 &&
+        		$item->getUrl() == 'enable-cookies')
+        			continue;
+            $xml = sprintf('<url><loc>%s</loc><lastmod>%s</lastmod><changefreq>%s</changefreq><priority>%.1f</priority></url>',
+                htmlspecialchars($baseUrl . $item->getUrl()),
+                $date,
+                $changefreq,
+                $priority
+            );
+            $io->streamWrite($xml);
+        }
+        unset($collection);
+
+        $io->streamWrite('</urlset>');
+        $io->streamClose();
+
+        $this->setSitemapTime(Mage::getSingleton('core/date')->gmtDate('Y-m-d H:i:s'));
+        $this->save();
+
+        return $this;
+    }
+}
